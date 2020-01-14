ba3f22282c27d9d94499645decb6b6bcf729ab6e
Types Logics Cats <TheAspiringHacker@protonmail.com>
2020-01-14 10:06:18 -0500

    Update executable to interpret programs instead of outputting types

--- app/main.ml
+++ app/main.ml
@@ -1,4 +1,4 @@
-(* Copyright (C) 2018-2019 Types Logics Cats.
+(* Copyright (C) 2018-2020 Types Logics Cats.
 
    This Source Code Form is subject to the terms of the Mozilla Public
    License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -8,26 +8,18 @@ open Base
 open Stdio
 open Emmeline
 
+let main_prefix = { Qual_id.Prefix.package = ""; path = [] }
+
 let () =
-  let open Result.Monad_infix in
   match
     let lexbuf = Lexing.from_channel stdin in
-    let ast = Parser.expr_eof Lexer.expr lexbuf in
-    let package = Package.create { Qual_id.Prefix.package = ""; path = [] } in
-    let env = Env.empty (module String) in
-    let packages = Hashtbl.create (module Qual_id.Prefix) in
-    let desugarer = Desugar.create package packages in
-    let typechecker = Typecheck.create package packages in
-    Desugar.term_of_expr desugarer typechecker env ast
-    >>= fun term ->
-    Typecheck.infer_term typechecker term
-    >>| fun typedtree ->
-    Typecheck.gen typechecker typedtree.Typedtree.ty
+    Parser.file Lexer.expr lexbuf
+    |> Pipeline.compile (Hashtbl.create (module Qual_id.Prefix)) main_prefix
   with
-  | Ok ty ->
-     let pprinter = Prettyprint.create () in
-     Prettyprint.print_polytype pprinter ty;
-     print_endline (Prettyprint.to_string pprinter)
+  | Ok asm_module ->
+     let vm = Eval.create (Hashtbl.create (module Qual_id.Prefix)) in
+     Io.init Io.stdio vm;
+     ignore (Eval.eval vm asm_module)
   | Error e ->
      let pp = Prettyprint.create () in
      Prettyprint.print_message Prettyprint.print_span pp e;
